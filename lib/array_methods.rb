ce5f5b9bdc90bff985fc1bb9db5d912ae808a04d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 13:13:01 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,25 @@
 def using_include(array, element)
-
+   array.include?(element) ? true : false
 end
 
 def using_sort(array)
-
+   new_sorted = array.sort
+   new_sorted
 end
 
 def using_reverse(array)
-
+   new_reversed = array.reverse
+   new_reversed
 end
 
 def using_first(array)
-
+   array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+   array.size
 end
